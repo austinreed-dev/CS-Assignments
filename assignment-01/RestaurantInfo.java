5e85744103585ea772621c06fc62fb937f3985af
Austin <austin.m.reed1@outlook.com>
2026-02-08 21:19:34 -0600

    Added first assignment that was never turned in

---
+++ assignment-01/RestaurantInfo.java
@@ -0,0 +1,15 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        // Declaring variables for the restaurant details
+        String name = "The Golden Grill";
+        String address = "123 Main Street";
+        String cityStateZip = "Denver, CO, 80202";
+
+        // Printing the information to the console
+        System.out.println("--- Restaurant Information ---");
+        System.out.println("Restaurant Name:  " + name);
+        System.out.println("Business Address: " + address);
+        System.out.println("City, State, Zip: " + cityStateZip);
+        System.out.println("------------------------------");
+    }
+}
